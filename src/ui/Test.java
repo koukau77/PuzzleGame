8732faf9b90add96f887bc30c17c158bfd6f2ca7
koukau77 <koukau77@gmail.com>
2024-04-23 14:54:21 +0900

    ui

--- src/ui/Test.java
+++ src/ui/Test.java
@@ -1,4 +1,20 @@
 package ui;
 
+import javax.swing.*;
+
 public class Test {
+    public static void main(String[] args) {
+        JFrame gameFrame = new JFrame();
+        gameFrame.setSize(603,680);
+        gameFrame.setVisible(true);
+
+        JFrame loginFrame = new JFrame();
+        loginFrame.setSize(488,430);
+        loginFrame.setVisible(true);
+
+        JFrame registerFrame = new JFrame();
+        registerFrame.setSize(488,500);
+        registerFrame.setVisible(true);
+
+    }
 }
